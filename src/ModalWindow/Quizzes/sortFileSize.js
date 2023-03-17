ef65a8aa78c178dc86ba942696c1fd374fcdf35f
User Name <email@example.com>
2023-03-17 22:04:56 +0100

    sliders

--- src/ModalWindow/Quizzes/sortFileSize.js
+++ src/ModalWindow/Quizzes/sortFileSize.js
@@ -1,90 +1,68 @@
 import { useState } from "react";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
-import "./DnD.css";
 import { motion } from "framer-motion";
-/*https://github.com/thebikashweb/react-drag-drop-without-library/tree/master/src*/
-/*bez pouziti knihovny*/
-const finalFiles = [
-  {
-    id: "fotka",
-    name: "obrazek.jpeg",
-    thumb: "/images/kvn.png",
-  },
-  {
-    id: "text",
-    name: "text.txt",
-    thumb: "/images/cz-01.png",
-  },
-  {
-    id: "video",
-    name: "video.mp4",
-    thumb: "/images/gary.png",
-  },
-  {
-    id: "hudba",
-    name: "hudba.mp3",
-    thumb: "/images/gary.png",
-  },
-];
-function CheckCorectness(i) {
-  if (
-    i[0].name == "video.mp4" &&
-    i[1].name == "hudba.mp3" &&
-    i[2].name == "obrazek.jpeg" &&
-    i[3].name == "text.txt"
-  ) {
-    var modal = document.getElementById("modal-window32");
-    modal.style.display = "block";
+
+function SortFileSize() {
+  function CheckCorectness() {
+    const deviation = 100;
+    const count1 = 100;
+    const count2 = 100;
+    const count3 = 100;
+    const count4 = 100;
+    if (
+      Math.abs(value1 - count1) < deviation &&
+      Math.abs(value2 - count2) < deviation &&
+      Math.abs(value3 - count3) < deviation &&
+      Math.abs(value4 - count4) < deviation
+    ) {
+      var modal = document.getElementById("modal-window32");
+      modal.style.display = "block";
+    }
   }
-}
-function SortFileSize(props) {
-  const [files, updateCharacters] = useState(finalFiles);
+  const [value1, setValue1] = useState(500); // set the initial value of the slider to 500
+  const [value2, setValue2] = useState(500); // set the initial value of the slider to 500
+  const [value3, setValue3] = useState(500); // set the initial value of the slider to 500
+  const [value4, setValue4] = useState(500); // set the initial value of the slider to 500
 
-  function handleOnDragEnd(result) {
-    if (!result.destination) return;
-
-    const items = Array.from(files);
-    const [reorderedItem] = items.splice(result.source.index, 1);
-    items.splice(result.destination.index, 0, reorderedItem);
-
-    updateCharacters(items);
+  function handleChange1(event) {
+    setValue1(event.target.value);
+  }
+  function handleChange2(event) {
+    setValue2(event.target.value);
+  }
+  function handleChange3(event) {
+    setValue3(event.target.value);
+  }
+  function handleChange4(event) {
+    setValue4(event.target.value);
   }
 
   return (
     <>
       <div className="container-sort">
-        <div className="left-column">
-          <DragDropContext onDragEnd={handleOnDragEnd}>
-            <Droppable droppableId="characters">
-              {(provided) => (
-                <ul
-                  className="characters"
-                  {...provided.droppableProps}
-                  ref={provided.innerRef}
-                >
-                  {files.map(({ id, name }, index) => {
-                    return (
-                      <Draggable key={id} draggableId={id} index={index}>
-                        {(provided) => (
-                          <li
-                            ref={provided.innerRef}
-                            {...provided.draggableProps}
-                            {...provided.dragHandleProps}
-                          >
-                            <p>{name}</p>
-                          </li>
-                        )}
-                      </Draggable>
-                    );
-                  })}
-                  {provided.placeholder}
-                </ul>
-              )}
-            </Droppable>
-          </DragDropContext>
+        <div className="first-line">
+          <RangeSlider
+            heading="video"
+            value={value1}
+            handleChange={handleChange1}
+          />
+          <RangeSlider
+            heading="text"
+            value={value2}
+            handleChange={handleChange2}
+          />
+          <RangeSlider
+            heading="fotka"
+            value={value3}
+            handleChange={handleChange3}
+          />
+          <RangeSlider
+            heading="obrázek"
+            value={value4}
+            handleChange={handleChange4}
+          />
         </div>
 
-        <div className="right-column">
+        <div className="second-line">
           <div className="sort-files-button">
             <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.9 }}>
               <a
@@ -95,10 +73,7 @@ function SortFileSize(props) {
               </a>
             </motion.div>
           </div>
-          <div
-            className="sort-files-button"
-            onClick={() => CheckCorectness(files)}
-          >
+          <div className="sort-files-button" onClick={() => CheckCorectness()}>
             <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.9 }}>
               ZKONTROLUJ
             </motion.div>
@@ -106,6 +81,22 @@ function SortFileSize(props) {
         </div>
       </div>
     </>
+  );
+}
+
+function RangeSlider({ value, handleChange, heading }) {
+  return (
+    <div className="sort-file-input">
+      <h3>{heading}</h3>
+      <input
+        type="range"
+        min="0"
+        max="1000"
+        value={value}
+        onChange={handleChange}
+      />
+      <p>{value}</p>
+    </div>
   );
 }
 
