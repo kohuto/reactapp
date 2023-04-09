0d399110c8d8a74c2d9a73f33244f7855747c319
User Name <email@example.com>
2023-04-09 14:22:47 +0200

    summary review finished

--- src/Data/Quizzes/dataSummary.js
+++ src/Data/Quizzes/dataSummary.js
@@ -5,60 +5,64 @@ export const summaryText = [
     prerequsities: ["Zatím nic."],
     learningOutcomes: [
       "Internet, není nic neviditelného.",
-      "Kam se ukládá vše, co najdeme na internetu?",
-      "Jak fungují zařízení jako server a chytrá křižovatka?",
-      "Proč spolu server a klient potřebují komunikovat?",
-      "Kudy putují všechna data?",
+      "Kam se ukládá vše, co najdeme na internetu.",
+      "Jak fungují zařízení jako server a chytrá křižovatka.",
+      "Proč spolu servery a klienti potřebují komunikovat.",
+      "Kudy putují všechna data.",
     ],
   },
   {
     chapterSummary:
-      "V předchozí kapitole jsme si ukázali jednotlivá zařízení, ze kterých se skládá internet. V této kapitole se blíže podíváme na data, která si mezi sebou tato zařízení posílají.",
+      "V předchozí kapitole jsi viděl zařízení, která tvoří internet. V této kapitole se dozvíš více o datech, která si mezi sebou zařízení posílají.",
     prerequsities: [
-      "víme, jaké jsou základní prvky, ze kterých se skládá počítačová síť, tedy pojmy klient, server, chytrá křižovatka a cesta pro informace",
-      "víme, jak spolu zařízení vzájemně komunikují",
+      "Internet je tvořen klienty, servery, chytrými křižovatkami či cestami pro informace.",
+      "Zařízení navzájem komunikují.",
     ],
     learningOutcomes: [
-      "Co je paket",
-      "Co musí paket obsahovat, aby byl schopen doručit data na správné místo",
-      "jaký je rozdíl, když kamarádovi pošleš video nebo textovou zprávu",
+      "Co jsou pakety.",
+      "Co obsahují pakety, aby byla data správně doručena.",
+      "Jaký je rozdíl, mezi posláním videa a textové zprávy.",
     ],
   },
   {
     chapterSummary:
-      "Už rozumíme tomu, jak vypadají data, která posíláme. Proto se v této kapitole se zaměříme na to, jak data vlastně můžeme přenášet.",
+      "Už víš, jak vypadají přenášená data. V této kapitole se dozvíš, jak lze data přenášet.",
     prerequsities: [
-      "víme, jaké jsou základní prvky, ze kterých se skládá počítačová síť, tedy pojmy klient, server, chytrá křižovatka a cesta pro informace",
-      "víme, že informace jsou přenášeny v paketech",
+      "Internet je tvořen klienty, servery, chytrými křižovatkami či cestami pro informace.",
+      "Data jsou přenášená v paketech.",
     ],
     learningOutcomes: [
-      "ukážeme si různé způsoby, jak se dají data na internetu přenášet",
-      "ukážeme si různé typy bezdrátových připojení",
-      "blíže si ukážeme, jak fungují kabely",
-      "prozkoumáme vlastnosti jednotlivých typů připojení",
+      "Jak jsou data přenášena.",
+      "Různé typy bezdrátových připojení.",
+      "Jak fungují kabely.",
+      "Vlastnosti jednotlivých typů připojení.",
     ],
   },
   {
     chapterSummary:
-      "Už známe jednotlivá zařízení, ze kterých se skládá internet. Víme jak spolu komunikují a víme jak vypadají data, která si zařízení navzájem posílají. V této kapitole se více zaměříme na přenos, popíšeme si tedy, jak přesně vypadá cesta paketů do cíle a jaké problémy mohou po cestě nastat.",
+      "Už znáš zařízení, ze kterých se skládá internet. Víme jak vypadají data a jak se posílají. V této kapitole se dozvíš více o cestování mezi odesílatelem a příjemcem.",
     prerequsities: [
-      "víme, jaké jsou základní prvky, ze kterých se skládá počítačová síť, tedy pojmy klient, server, chytrá křižovatka a cesta pro informace",
-      "víme, že informace jsou přenášeny v paketech",
+      "Internet je tvořen klienty, servery, chytrými křižovatkami či cestami pro informace.",
+      "Data jsou přenášená v paketech.",
     ],
     learningOutcomes: [
-      "Vysvětlíme si, co je IP adresa a k čemu slouží",
-      "Ukážeme si rozdíl mezi dvěma typy IP adres - IPv4 a IPv6",
-      "Rozebereme jednotlivé části cesty paketu od odesílatele až k příjemci",
-      "Zjistíme, že cesta paketů není náhodná",
-      "Zjistíme, proč je dobré, aby pakety putovaly nezávisle na sobě",
+      "K čemu slouží IP adresa.",
+      "Jaký je rozdíl mezi IPv4 a IPv6 adresami.",
+      "Jaké vlastnosti by měla mít internetová síť.",
+      "Jak se určí cesta paketů.",
     ],
   },
   {
-    chapterSummary: "umíme vše",
-    prerequsities: ["všechno"],
-    learningOutcomes: [
-      "ověříš si, jestli jsi všemu porozuměl",
-      "ověříš si, jestli jsi schopen nabyté znalosti propojit",
+    chapterSummary:
+      "Už víš vše o tom, jak funguje internet. V této kapitole postavíš několik internetových sítí podle různých zadání. Musíš ale myslet na vše, co ses naučil.",
+    prerequsities: [
+      "Internet je tvořen klienty, servery, chytrými křižovatkami či cestami pro informace.",
+      "Jaké vlastnosti by měla mít internetová síť.",
+      "Jak jsou data přenášena.",
+      "Různé typy bezdrátových připojení.",
+      "Jak fungují kabely.",
+      "Vlastnosti jednotlivých typů připojení.",
     ],
+    learningOutcomes: ["Už vše umíš."],
   },
 ];
